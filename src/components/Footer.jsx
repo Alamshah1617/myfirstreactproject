fcf6ceb1857ac1abe74a4c0753cfa3eb1e4c03e2
Shah Alam <alamshah1617@gmail.com>
2023-09-06 19:31:29 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>EleenaTech</h1>
+        <p>@all right reserved 2023</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com/@shahalam_" target={"blank"}>Youtube</a>
+          <a href="https://instagram.com/shah_alam00" target={"blank"}>Instagram</a>
+          <a href="https://github.com/Alamshah1617" target={"blank"}>Github</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
